4c7c8777d413bd78d0e2356fd9089a66694a0967
azgk <76268212+azgk@users.noreply.github.com>
2023-02-05 09:55:48 -0500

    Found a bug when testing on more CSV files -- because strings were compares, "12/26/21" turned out to be bigger than "01/03/23" causing an error in filtering dataframe for current month. Thus edited eom.modify_df by converting dates in CSV to datetime for date comparisons.

--- date_ranges.py
+++ date_ranges.py
@@ -6,9 +6,9 @@ class DateRange:
   def __init__(self, year: int = None, month: int = None):
     self.year = year
     self.month = month
-    self.month_begin = datetime(year=self.year, month=self.month, day=1).strftime("%m/%d/%Y")
+    self.month_begin = datetime(year=self.year, month=self.month, day=1)
     try:
-      self.next_month_begin = datetime(year=self.year, month=self.month + 1, day=1).strftime("%m/%d/%Y")
+      self.next_month_begin = datetime(year=self.year, month=self.month + 1, day=1)
     except ValueError:
       # Exception is for Dec.
-      self.next_month_begin = datetime(year=self.year + 1, month=1, day=1).strftime("%m/%d/%Y")
+      self.next_month_begin = datetime(year=self.year + 1, month=1, day=1)
